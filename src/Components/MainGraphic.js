1e612d5ca240043f7dfae57b240cbd4805c74bde
Manav1011 <manavshah1011.ms@gmail.com>
2024-03-22 17:56:27 +0530

    Added new backgrounds

--- src/Components/MainGraphic.js
+++ src/Components/MainGraphic.js
@@ -1,33 +1,83 @@
 export default function MainGraphic() {
-    return (
-      <div id="firstfold"> 
-      <div className="flex flex-col sm:hidden" style={{backgroundAttachment:'fixed',backgroundImage:"url('/images/backgrounds/GTU_CAMPUS_POTRAIT_4.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", width: "100%", height: "100vh"}}>
-        <div className="h-full w-full flex flex-col backdrop-filter" style={{backdropFilter:'blur(1px)'}}>                    
-            <div className="slide-right bg-gray-900 mt-32 mr-10 text-left bg-clip-padding bg-clip-padding backdrop-filter rounded-r-full bg-opacity-75 p-5">
-                <h1 className="text-orange-500 my-2 leading-10 sm:leading-1 text-2xl font-bold" style={{fontFamily:'Azonix'}}>Igniting Innovation</h1>        
-            </div>  
-            <div className="slide-right bg-gray-900 mt-10 mr-10 text-left bg-clip-padding bg-clip-padding backdrop-filter rounded-r-full bg-opacity-75 p-5">
-                <h1 className="text-slate-300 my-2 leading-10 sm:leading-1 text-md font-bold" style={{fontFamily:'Azonix'}}>Empowering Entrepreneurs</h1>
+  return (
+    <div id="firstfold">
+      <div
+        className="flex flex-col sm:hidden"
+        style={{
+          backgroundAttachment: "fixed",
+          backgroundImage:
+            "url('/images/backgrounds/GTU_CAMPUS_POTRAIT_4.jpg')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div
+          className="h-full w-full flex flex-col backdrop-filter"
+          style={{ backdropFilter: "blur(1px)",}}
+        >
+          <div className="pt-40 pb-40 bg-white slide-right mb-2 text-center bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30" style={{clipPath:'circle(50% at 50% 50%)',backgroundImage:`url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`}}>          
+            <h1
+              className=" text-orange-500 my-2 leading-10 sm:leading-1 font-bold"
+              style={{ fontFamily: "Azonix" ,fontSize:'2em'}}
+            >
+              Igniting Innovation
+            </h1>
+            <h1
+              className="text-slate-300 my-2 leading-10 sm:leading-1 text-md font-bold"
+              style={{ fontFamily: "Azonix",fontSize:'1.1em'}}
+            >
+              Empowering Entrepreneurs
+            </h1>
+            <div
+              className="font-semibold	sm:font-normal text-green-500"
+              style={{ fontFamily: "dosislight",fontSize:'0.8em'}}
+            >
+              Welcome to the Gateway of Opportunities at GTU Innovation Council
             </div>
-            <div className="slide-right bg-gray-900 mt-10 mr-10  text-left bg-clip-padding bg-clip-padding backdrop-filter rounded-r-full bg-opacity-75 p-5">
-                <div className="font-semibold sm:font-normal text-green-500" style={{fontFamily:'dosislight',fontSize:'15px'}}>
-                        Welcome to the Gateway of Opportunities at GTU Innovation Council
-                </div>   
-            </div>
-            </div>        
-      </div>
-      <div className="hidden sm:flex backdrop-filter bg-opacity-5" style={{backgroundAttachment:'fixed',backgroundImage:"url('/images/backgrounds/GTU_CAMPUS_6.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", width: "100%", height: "100vh"}}>
-        <div className="h-full w-full flex flex-col bg-gray-900  backdrop-filter bg-opacity-20  justify-end items-start" style={{backdropFilter:'blur(1px)'}}>            
-        <div className="slide-right bg-gray-900 mb-2 text-left bg-clip-padding backdrop-filter rounded-r-full backdrop-blur-sm bg-opacity-75 p-10">
-            <h1 className="text-orange-500 my-2 leading-10 sm:leading-1 text-4xl font-bold" style={{fontFamily:'Azonix'}}>Igniting Innovation</h1>
-            <h1 className="text-slate-300 my-2 leading-10 sm:leading-1 text-4xl font-bold" style={{fontFamily:'Azonix'}}>Empowering Entrepreneurs</h1>
-            <div className="font-semibold	sm:font-normal text-green-500 text-2xl" style={{fontFamily:'dosislight'}}>
-                Welcome to the Gateway of Opportunities at GTU Innovation Council
-            </div>                                    
+          </div>
         </div>
-        </div>        
       </div>
+      <div
+        className="hidden sm:flex backdrop-filter bg-opacity-5"
+        style={{
+          backgroundAttachment: "fixed",
+          backgroundImage: "url('/images/backgrounds/GTU_CAMPUS_6.jpg')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div
+          className="h-full w-full flex flex-col bg-gray-900  backdrop-filter bg-opacity-20  justify-end items-start"
+          style={{ backdropFilter: "blur(1px)" }}
+        >
+          <div className="slide-right bg-gray-900 mb-2 text-left bg-clip-padding backdrop-filter rounded-r-full backdrop-blur-sm bg-opacity-75 p-10">
+            <h1
+              className="text-orange-500 my-2 leading-10 sm:leading-1 text-4xl font-bold"
+              style={{ fontFamily: "Azonix" }}
+            >
+              Igniting Innovation
+            </h1>
+            <h1
+              className="text-slate-300 my-2 leading-10 sm:leading-1 text-4xl font-bold"
+              style={{ fontFamily: "Azonix" }}
+            >
+              Empowering Entrepreneurs
+            </h1>
+            <div
+              className="font-semibold	sm:font-normal text-green-500 text-2xl"
+              style={{ fontFamily: "dosislight" }}
+            >
+              Welcome to the Gateway of Opportunities at GTU Innovation Council
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/Components/SecondFold.js
+++ src/Components/SecondFold.js
@@ -23,7 +23,7 @@ const Page = ({
   content,
 }) => (
   <>
-    <ParallaxLayer offset={offset} speed={0.2} onClick={onClick} id={id}>
+    <ParallaxLayer offset={offset} speed={0.1} id={id}>
       <div className={`${styles.slopeEnd} ${styles[gradient]}`} />
     </ParallaxLayer>
     <ParallaxLayer
@@ -32,26 +32,34 @@ const Page = ({
       className={`${styles.text}`}
       id="secondfolds"
     >
-      <div className="sm:mt-0 flex justify-center flex-col sm:flex-row items-center h-screen">
-        <div className="w-full h-48  sm:w-1/4 text-center ">
+      <div className="flex justify-center flex-col sm:flex-row items-center h-screen">
+        {logo && <div className="w-full h-48  sm:w-1/4 text-center ">
           <img
             src={logo}
             alt="Leafs"
             className="mx-auto"
             style={{ width: "300px", height: "auto" }}
           />
-        </div>
+        </div>}
         <div className="order-1 sm:order-2w-full sm:w-8/12">
           <div className="container mx-auto h-full sm:p-10">
             <header className="container px-4 lg:flex mt-2 items-center h-full lg:mt-0">
               <div className="w-full">
-                <h1 className="text-4xl lg:text-6xl font-bold">{header}</h1>
+                <h1 className="uppercase text-2xl lg:text-6xl sm:text-3xl font-bold">{header}</h1>
                 <div className="w-20 h-2 bg-green-700 my-4" />
-                <p className="text-xl mb-10">{content}</p>
+                <p className="text-lg lg:text-xl mb-10">{content}</p>
               </div>
             </header>
           </div>
         </div>
+          <div className="order-3 flex flex-row justify-center cursor-pointer items-center"  onClick={onClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+            <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
+          </svg>
+          </div>
       </div>
     </ParallaxLayer>
   </>
@@ -74,10 +82,10 @@ export default function SecondFold() {
         id="secondfold"
         horizontal
         className="mt-20"
-        style={{
-          backgroundAttachment: "fixed",
-          backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
-        }}
+        // style={{
+        //   backgroundAttachment: "fixed",
+        //   backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+        // }}
       >
         <Page
           id="secondfoldfirstview"
@@ -96,7 +104,7 @@ export default function SecondFold() {
           offset={1}
           gradient="teal"
           onClick={() => scroll(2)}
-          logo={"/images/secondfold/IPR.png"}
+          // logo={"/images/secondfold/IPR.png"}
           header={"Intellectual property right"}
           content={
             <>
@@ -108,7 +116,7 @@ export default function SecondFold() {
           offset={2}
           gradient="tomato"
           onClick={() => scroll(3)}
-          logo={"/images/secondfold/NODALINSTITUTE.png"}
+          // logo={"/images/secondfold/NODALINSTITUTE.png"}
           header={"Nodal Institutes"}
           content={
             <>
@@ -121,7 +129,7 @@ export default function SecondFold() {
           offset={3}
           gradient="teal"
           onClick={() => scroll(0)}
-          logo={"/images/secondfold/DIC.png"}
+          // logo={"/images/secondfold/DIC.png"}
           header={"Design Innovation Centre"}
           content={
             <>
