2b28ef6f199d3c4887262179c49e705b3c6efa1d
AMAN-B-H-A-T-T <22csama003@ldce.ac.in>
2024-04-06 13:02:46 +0530

    admin side dashboard layout completed

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -1,6 +1,92 @@
 function page() {
   return (
-    <div className="w-full bg-black">Manav</div>
+    // <div className="w-full bg-black text-white">Manav</div>
+    <>
+      <div className="p-4 sm:ml-64">
+        <div className="p-3 rounded-lg dark:border-gray-700 mt-14">
+          <div className="header mb-3">
+            <span className="text-2xl">Models / Incubation Form</span>
+          </div>
+          <div className="flex h-full mb-4 rounded bg-gray-50 flex-col">
+            <div className="header px-2 py-2  mb-3 bg-gray-400 rounded w-full">
+              <span>Form Heading</span>
+            </div>
+            <div className="form w-full">
+              <form className="flex flex-col w-full">
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Select your country</label>
+                  <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>France</option>
+                    <option>Germany</option>
+                  </select>
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                  <input type="email" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Select your country</label>
+                  <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>France</option>
+                    <option>Germany</option>
+                  </select>
+
+                </div>
+                <div class="mb-5 w-full px-3">
+                  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Select your country</label>
+                  <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                </div>
+                <div class="mb-5 w-full px-3">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-black" for="user_avatar">Upload file</label>
+                <input type="file" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5 w-full px-3">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-black" for="user_avatar">Upload file</label>
+                <input type="file" id="email" class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full border border-gray-300" placeholder="name@flowbite.com" required />
+                </div>
+                <div className="w-full mb-5 px-3">
+                <button type="button" class=" w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Blue</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
