fc3e557cfa41a50cf7c2855af69ef8d6694a78d0
Manav1011 <manavshah1011.ms@gmail.com>
2024-06-23 16:36:51 +0530

    updates sixth fold

--- src/Components/SixthFold/layout.js
+++ src/Components/SixthFold/layout.js
@@ -2,65 +2,66 @@ import React from "react";
 
 async function SixthFold() {
   return (
-    <section className="h-full w-full" style={{backgroundImage:"url('/images/backgrounds/startup.webp')",backgroundPosition:'center',backgroundSize:'cover',backgroundAttachment:'fixed'}}>
-    <div className="min-h-screen py-20 bg-slate-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30  sm:py-32 0">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:max-w-none">
-          <div className="text-center space-y-4">
-            <h2 className="text-3xl sm:text-7xl font-extrabold tracking-tight text-slate-700 ">
-                STARTUP STATISTICS
-            </h2>
-            <p className="text-lg leading-8 sm:text-3xl text-slate-700">
-                The success story of GIC. In facts & numbers.
-            </p>
-          </div>
-          <dl className="mt-8 sm:mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
-            <div className="flex flex-col bg-black p-4 bg-opacity-90">
-              <dt className="text-sm font-semibold leading-6 text-slate-300">
-                Application Received SSIP + NI (2500 + 150)
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-white">
-              2,650 
-              </dd>
-            </div>
-            <div className="flex flex-col bg-white p-4 bg-opacity-90">
-              <dt className="text-sm font-semibold leading-6 text-slate-700">
-                Selected Startup Data SSIP + NI (250 + 26)
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-black">
-                276
-              </dd>
-            </div>
-            <div className="flex flex-col bg-black bg-opacity-90 p-8">
-            <dt className="text-sm font-semibold leading-6 text-slate-300">
-              Fund Disbursed in Lakh SSIP + NI (62 + 176 )  
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-white">
-                234
-              </dd>
-            </div>
-            <div className="flex flex-col bg-white p-4 bg-opacity-90">
-              <dt className="text-sm font-semibold leading-6 text-slate-700">
-                Jobs Created by Startup (SSIP +NI)
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight black">
-                500
-              </dd>
-            </div>
-            <div className="flex flex-col bg-black sm:bg-slate-200 bg-opacity-90 p-8"  style={{gridColumn:'1 / -1'}}>
-            <dt className="text-sm font-semibold leading-6 text-slate-300 sm:text-slate-700">
-                Jobs Created by Startup (SSIP +NI)
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-white sm:text-black">
-                500
-              </dd>
-            </div>
+    <></>
+    // <section className="h-full w-full" style={{backgroundImage:"url('/images/backgrounds/startup.webp')",backgroundPosition:'center',backgroundSize:'cover',backgroundAttachment:'fixed'}}>
+    // <div className="min-h-screen py-20 bg-slate-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30  sm:py-32 0">
+    //   <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    //     <div className="mx-auto max-w-2xl lg:max-w-none">
+    //       <div className="text-center space-y-4">
+    //         <h2 className="text-3xl sm:text-7xl font-extrabold tracking-tight text-slate-700 ">
+    //             STARTUP STATISTICS
+    //         </h2>
+    //         <p className="text-lg leading-8 sm:text-3xl text-slate-700">
+    //             The success story of GIC. In facts & numbers.
+    //         </p>
+    //       </div>
+    //       <dl className="mt-8 sm:mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+    //         <div className="flex flex-col bg-black p-4 bg-opacity-90">
+    //           <dt className="text-sm font-semibold leading-6 text-slate-300">
+    //             Application Received SSIP + NI (2500 + 150)
+    //           </dt>
+    //           <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+    //           2,650 
+    //           </dd>
+    //         </div>
+    //         <div className="flex flex-col bg-white p-4 bg-opacity-90">
+    //           <dt className="text-sm font-semibold leading-6 text-slate-700">
+    //             Selected Startup Data SSIP + NI (250 + 26)
+    //           </dt>
+    //           <dd className="order-first text-3xl font-semibold tracking-tight text-black">
+    //             276
+    //           </dd>
+    //         </div>
+    //         <div className="flex flex-col bg-black bg-opacity-90 p-8">
+    //         <dt className="text-sm font-semibold leading-6 text-slate-300">
+    //           Fund Disbursed in Lakh SSIP + NI (62 + 176 )  
+    //           </dt>
+    //           <dd className="order-first text-3xl font-semibold tracking-tight text-white">
+    //             234
+    //           </dd>
+    //         </div>
+    //         <div className="flex flex-col bg-white p-4 bg-opacity-90">
+    //           <dt className="text-sm font-semibold leading-6 text-slate-700">
+    //             Jobs Created by Startup (SSIP +NI)
+    //           </dt>
+    //           <dd className="order-first text-3xl font-semibold tracking-tight black">
+    //             500
+    //           </dd>
+    //         </div>
+    //         <div className="flex flex-col bg-black sm:bg-slate-200 bg-opacity-90 p-8"  style={{gridColumn:'1 / -1'}}>
+    //         <dt className="text-sm font-semibold leading-6 text-slate-300 sm:text-slate-700">
+    //             Jobs Created by Startup (SSIP +NI)
+    //           </dt>
+    //           <dd className="order-first text-3xl font-semibold tracking-tight text-white sm:text-black">
+    //             500
+    //           </dd>
+    //         </div>
         
-          </dl>
-        </div>
-      </div>
-    </div>
-    </section>
+    //       </dl>
+    //     </div>
+    //   </div>
+    // </div>
+    // </section>
   );
 }
 
