d5adcb7eb6d5a27bc9385df4a1afee721aa30697
Manav1011 <manavshah1011.ms@gmail.com>
2024-06-30 20:28:53 +0530

    Moved db connection to the instrumentation

--- src/app/(admin)/Model/User.js
+++ src/app/(admin)/Model/User.js
@@ -1,32 +1,33 @@
-const mongoose = global.mongoose
-const User = new mongoose.Schema(
-    {
-        user_name : {
-            type : String,
-            required : true
+const mongoose = global.mongoose;
+
+if (mongoose) {
+    const UserSchema = new mongoose.Schema({
+        user_name: {
+            type: String,
+            required: true
         },
-        user_email : {
-            type : String,
-            required : true,
-            unique : true 
+        user_email: {
+            type: String,
+            required: true,
+            unique: true
         },
-        user_contact : {
-            type : String,
-            required : true
+        user_contact: {
+            type: String,
+            required: true
         },
-        user_password : {
-            type : String,
-            required : true,
+        user_password: {
+            type: String,
+            required: true
         },
-        session_id : {
-            type : String,
+        session_id: {
+            type: String,
         },
-        is_session_active : {
-            type : String,
-            default : false
+        is_session_active: {
+            type: Boolean,
+            default: false
         }
-    }
-);
-export default mongoose.models.User || mongoose.model("User", User);
+    });
 
-
+    // Export the User model or use the existing one if it already exists
+    module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
+}
