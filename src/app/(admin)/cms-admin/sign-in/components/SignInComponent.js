7570ec2c50176382888be53dbaf70a4485f54296
Manav1011 <manavshah1011.ms@gmail.com>
2024-06-30 19:39:59 +0530

    updated footer and about section

--- src/app/(admin)/cms-admin/sign-in/components/SignInComponent.js
+++ src/app/(admin)/cms-admin/sign-in/components/SignInComponent.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { SignInUser, AuthenticateUser } from "./actions";
+import { SignInUser, AuthenticateUser } from "../actions";
 import { useFormState } from "react-dom";
 
 function SignInComponent() {
