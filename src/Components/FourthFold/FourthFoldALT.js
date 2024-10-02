9a009b461f5793e7b61f4fbe284f000ce06cc306
AMAN-B-H-A-T-T <22csama003@ldce.ac.in>
2024-10-02 18:47:16 +0530

    changes on 2/10

--- src/Components/FourthFold/FourthFoldALT.js
+++ src/Components/FourthFold/FourthFoldALT.js
@@ -64,7 +64,7 @@ async function FourthFoldALT({circulars}) {
           <div className="mx-5 sm:mx-auto" style={{ fontFamily: "AvenirLight" }}>
             {/* Item #1 */}
             {circulars.map((circular,index)=>(
-                 <div className="relative pl-8 sm:pl-32 py-6 group report opacity-0" key={index}>
+                 <div className="relative pl-8 sm:pl-32 py-6 group report opacity-0 max-w-[75%]" key={index}>
                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">
                      {circular.date}
@@ -73,7 +73,7 @@ async function FourthFoldALT({circulars}) {
                      Circular
                    </span>
                    <div className="text-xl font-bold text-slate-900">
-                     <a href={circular.url}>{circular.title}</a>
+                     <a href={circular.url} target="_blank">{circular.title}</a>
                    </div>
                  </div>
                  {/* Content */}
